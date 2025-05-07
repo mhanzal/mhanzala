eaee194a3010d8a2d3b910db3261df8f43213950
Muhammad Hanzala <mhanzala87654@gmail.com>
2025-05-07 11:58:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//     document.querySelector(".loading").style.display = "none"
+// }, 2000)
+
+AOS.init();
+
+// $(".btn").click(function () { 
+//     $("body").toggleClass("dark");
+// });
+// $(window).scroll(function () { 
+//     let a = $(window).scrollTop()
+//     if (a > 300){
+//         $(".btnfull").show()
+//     }else{
+//         $(".btnfull").hide()
+//     }
+// })
+
+// $(".btnfull").click(function(){
+//     $("body").animate({scrollTop: 0})
+// })
